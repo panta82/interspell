2fe12871e48606959c862de6da1fda175b64208d
Ivan Pantic <ivanpantic82@gmail.com>
2014-09-24 18:58:15 +0200

    Initial project structure

---
+++ README.md
@@ -0,0 +1,28 @@
+# Interspell
+
+----
+
+Very simple library to manipulate human-readable time intervals.
+
+Install with:
+
+```bash
+npm install interspell
+```
+
+Then use like this:
+
+```javascript
+    // You only need this one function.
+	var interspell = require("interspell");
+
+	// WIP
+```
+
+WIP
+
+----
+
+### Licence
+
+Apache v2. Read it [here](LICENSE).
---
+++ spec/interspell.spec.js
@@ -0,0 +1,16 @@
+var interspell = require("../lib/interspell");
+
+describe("Interspell", function () {
+
+	it("should xxx", function (done) {
+		/*expect(err.count).toEqual(1);
+		expect(err.C.message).toEqual("errC");
+
+		expect(res.count).toEqual(2);
+		expect(res.A).toEqual("resA");
+		expect(res.B).toEqual("resB");
+		expect(new Date() - startedAt).toBeLessThan(220);*/
+
+		done();
+	});
+});
